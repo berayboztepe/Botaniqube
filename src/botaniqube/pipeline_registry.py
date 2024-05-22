95b4f9b36f66d7d3f97da23ce7c3fe0787360b32
Emre Beray Boztepe <berayboztepe@outlook.com>
2024-05-22 14:04:23 +0200

    Update pipeline_registry.py
    Dataset downloading pipeline is added to pipeline

--- src/botaniqube/pipeline_registry.py
+++ src/botaniqube/pipeline_registry.py
@@ -1,7 +1,7 @@
 """Project pipelines."""
 from typing import Dict
 from kedro.pipeline import Pipeline
-from botaniqube.pipelines import dataset_loading_pipeline, training_pipeline, testing_pipeline
+from botaniqube.pipelines import dataset_loading_pipeline, training_pipeline, testing_pipeline, dataset_downloading_pipeline
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -10,14 +10,16 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
+    ddp = dataset_downloading_pipeline.create_pipeline()
     dlp = dataset_loading_pipeline.create_pipeline()
     trp = training_pipeline.create_pipeline()
     tsp = testing_pipeline.create_pipeline()
     
     return {
-        "__default__": dlp + trp + tsp,
+        "__default__": ddp + dlp + trp + tsp,
+        "dataset_downloading": ddp,
         "dataset_loading": dlp,
         "training": trp,
         "testing": tsp,
-        "dataset_loading_training": dlp + trp,
+        "dataset_loading_training": ddp + dlp + trp,
     }
